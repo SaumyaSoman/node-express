7c801edc4d741c7bae99b93ad1a909f981493138
SaumyaSoman <saumi89@gmail.com>
2015-09-02 17:32:38 -0400

    custom validation using mongoose

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
+var validate = require('mongoose-validate');
 
 var schema = mongoose.Schema({
-    	email: {type : String, unique:true}
+    	email: {type : String, required:true, unique:true, validate:[validate.email,'is not a valid email address']}
 	});
+
+schema.set('autoIndex', App.env!=='production');
 
 var Model=  mongoose.model('User', schema);
 
