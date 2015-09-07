03296ecb0b78cdbae91da63f04f78077a161a2f5
SaumyaSoman <saumi89@gmail.com>
2015-09-06 23:47:58 -0400

    supertest testing

--- test/routes/userRoutesTest.js
+++ test/routes/userRoutesTest.js
@@ -1,9 +1,81 @@
 require ('../testHelper.js');
 
 var User=App.model('user');
-
+var superTest=require('supertest');
 
 describe(__filename, function(){
+
+	var Test;
+
+	beforeEach(function(){
+		Test={};
+	});
+
+	describe("Having a user in the database", function(){
+
+		beforeEach(function(done){
+
+			Test.userData={
+			email:"abc@xyz.com",
+			password:"override"
+			};
+
+			Test.user=new User({email:Test.userData.email, passwordHash:Test.userData.password});
+
+			Test.user.save(function(err){
+				assert.ifError(err);
+				done();
+			});
+
+		});
+		
+		it("create user which valid email id", function(done){
+			superTest(App.app)
+			.post('/signup')
+			.send({email:'aa@aas.com', password:'aaa'})
+			//.expect(200)
+			.end(function(err,res){
+				if(err){
+					//console.log("error hmm:", err);
+					//console.log(res.body);
+					assert.ifError(err);
+				}else{
+					User.find({email:"aa@aas.com"},function (err,users) {
+						//console.log("error here");
+						assert.ifError(err);
+
+						assert(users[0]);
+						assert.equal(users[0].email,"aa@aas.com");
+						done();
+						
+					})
+				}
+			})
+		});
+		/*
+		it("create user with duplicate email id", function(done){
+			superTest(App.app)
+			.post('/signup')
+			.send({email:"abc@xyz.com", password:"override"})
+			.expect(422)
+			.end(function(err,res){
+				if(err){
+					console.log("error :", err);
+					console.log(res.body);
+					assert.ifError(err);
+				}else{
+					User.find({email:"aa@a.com"},function (err,users) {
+						done();
+						
+					})
+				}
+			})
+		})
+*/
+	})
+
+
+	
 	it("add a new user", function(done){
 		var userRoutes= App.routes('userRoutes');
 		var req={
